4c2ef3ef0a8389a8e53f2e409cfacba91cfa47d8
Revant Cherian Idikulay <RevantCI@users.noreply.github.com>
2019-11-29 11:39:21 +0530

    Update api.js
    point the api url to the vachan-api production server

--- src/store/api.js
+++ src/store/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://stagingapi.autographamt.com/v1/",
+  baseURL: "https://api.autographamt.com/v1/",
   timeout: 15000
 });
