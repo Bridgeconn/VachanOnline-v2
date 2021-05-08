01cee0b84a73ed4941959fc8fd05d280f6cd1f3d
AnuMonachan <anumonachan9953@gmail.com>
2021-05-08 12:19:42 +0530

    Updated OBS Code

--- src/components/stories/Stories.js
+++ src/components/stories/Stories.js
@@ -1,23 +1,23 @@
-import React from "react";
-import TopBar from "../read/TopBar";
-import Drawer from "@material-ui/core/Drawer";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useMemo, useEffect } from "react";
 import Markdown from "react-markdown";
-import MenuItem from "@material-ui/core/MenuItem";
+import rehypeHighlight from "rehype-highlight";
+import axios from "axios";
 import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import MenuItem from "@material-ui/core/MenuItem";
 import Slider from "@material-ui/core/Slider";
 import Select from "@material-ui/core/Select";
 import Menu from "@material-ui/core/Menu";
-import { BLUETRANSPARENT } from "../../store/colorCode";
 import Tooltip from "@material-ui/core/Tooltip";
 import FormControl from "@material-ui/core/FormControl";
 import AppBar from "@material-ui/core/AppBar";
 import Divider from "@material-ui/core/Divider";
-import rehypeHighlight from "rehype-highlight";
 import Link from "@material-ui/core/Link";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import axios from "axios";
+import TopBar from "../read/TopBar";
+import { BLUETRANSPARENT } from "../../store/colorCode";
 
 const drawerWidth = 400;
 
@@ -26,21 +26,16 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(1),
     minWidth: 200,
   },
-  menuHeader: {
-    color: BLUETRANSPARENT,
-    borderBottom: "2px solid #9a9a9a",
-  },
   menu: {
     textAlign: "center",
     width: "100%",
     display: "inline-block",
     fontSize: 18,
+    color: BLUETRANSPARENT,
   },
-
   margin: {
     height: theme.spacing(5),
   },
-
   settings: {
     padding: 0,
     width: "30px",
@@ -50,15 +45,8 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "20px",
     cursor: "pointer",
   },
-
-  toggle: {
-    marginTop: 60,
-  },
   root: {
     display: "flex",
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
   },
   drawer: {
     width: drawerWidth,
@@ -71,7 +59,6 @@ const useStyles = makeStyles((theme) => ({
   drawerHeader: {
     marginTop: 60,
   },
-
   stories: {
     paddingLeft: 20,
     paddingRight: 30,
@@ -91,87 +78,71 @@ const useStyles = makeStyles((theme) => ({
     overflow: "auto",
     fontSize: "1.2rem",
   },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
 }));
 
 const Stories = (props) => {
-  const bibleStoriesURL = process.env.REACT_APP_BIBLE_STORIES_URL;
-  console.log(bibleStoriesURL);
+  const API = useMemo(
+    () => axios.create({ baseURL: process.env.REACT_APP_BIBLE_STORIES_URL }),
+    []
+  );
   let { login, userDetails } = props;
-
   const classes = useStyles();
-  const [settingsAnchor, setSettingsAnchor] = React.useState(null);
-  const open = Boolean(settingsAnchor);
 
   const [storyId, setStoryId] = React.useState("01");
   const [lang, setLang] = React.useState("");
   const [stories, setStories] = React.useState();
   const [languageJson, setLanguageJson] = React.useState({});
   const [manifest, setManifest] = React.useState([]);
-  const [Languages, setLanguages] = React.useState([]);
+  const [languages, setLanguages] = React.useState([]);
   const [fontSize, setFontSize] = React.useState(20);
+  const [settingsAnchor, setSettingsAnchor] = React.useState(null);
+  const open = Boolean(settingsAnchor);
 
-  const handleSliderChange = (event, newValue) => {
-    setFontSize(newValue);
-  };
   function openSettings(event) {
     setSettingsAnchor(event.currentTarget);
   }
   function closeSettings() {
     setSettingsAnchor(null);
   }
+  const handleSliderChange = (event, newValue) => {
+    setFontSize(newValue);
+  };
 
   const getStory = (event) => {
-    let element = event.currentTarget;
-    let storyNum = element.getAttribute("data-id");
-    while (storyNum.length < 2) storyNum = "0" + storyNum;
+    event.preventDefault();
+    let storyNum = event.currentTarget.getAttribute("data-id");
+    if (storyNum.length < 2) storyNum = "0" + storyNum;
     setStoryId(storyNum);
   };
 
   const getLang = (event) => {
-    let element = event.currentTarget;
-    setLang(element.getAttribute("language_name"));
+    setLang(event.target.value);
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (lang !== "") {
-      axios({
-        method: "get",
-        url: bibleStoriesURL + lang + "/content/" + storyId + ".md",
-      }).then(function (response) {
-        let data = response.data;
-        setStories(data);
+      API.get(lang + "/content/" + storyId + ".md").then(function (response) {
+        setStories(response.data);
       });
     }
-  }, [bibleStoriesURL, storyId, setStories, lang]);
+  }, [API, storyId, lang]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (lang !== "") {
-      axios({
-        method: "get",
-        url: bibleStoriesURL + lang + "/manifest.json",
-      }).then(function (response) {
-        let data = response.data;
-        setManifest(data);
+      API.get(lang + "/manifest.json").then(function (response) {
+        setManifest(response.data);
       });
     }
-  }, [bibleStoriesURL, setManifest, lang]);
+  }, [API, lang]);
 
-  React.useEffect(() => {
-    axios({
-      method: "get",
-      url: bibleStoriesURL + "languages.json",
-    }).then(function (response) {
-      let data = response.data;
-      let languageArray = Object.keys(data);
+  useEffect(() => {
+    API.get("languages.json").then(function (response) {
+      let languageArray = Object.keys(response.data);
       setLanguages(languageArray);
-      setLanguageJson(data);
+      setLanguageJson(response.data);
       setLang(languageArray[0] || "");
     });
-  }, [bibleStoriesURL, setLanguages, setLanguageJson]);
+  }, [API]);
 
   return (
     <>
@@ -189,18 +160,9 @@ const Stories = (props) => {
         >
           <div className={classes.drawerHeader}>
             <FormControl variant="outlined" className={classes.formControl}>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={lang}
-              >
-                {Languages.map((text, y) => (
-                  <MenuItem
-                    key={y}
-                    value={text}
-                    language_name={text}
-                    onClick={(e) => getLang(e)}
-                  >
+              <Select value={lang} onChange={getLang}>
+                {languages.map((text, y) => (
+                  <MenuItem key={y} value={text}>
                     {languageJson[text]}
                   </MenuItem>
                 ))}
@@ -217,7 +179,6 @@ const Stories = (props) => {
               >
                 <i className="material-icons md-23">more_vert</i>
               </Tooltip>
-
               <Menu
                 id="long-menu"
                 anchorEl={settingsAnchor}
@@ -231,9 +192,8 @@ const Stories = (props) => {
                   },
                 }}
               >
-                <MenuItem className={`${classes.menuHeader} ${classes.menu}`}>
-                  Font Size
-                </MenuItem>
+                <MenuItem className={classes.menu}>Font Size</MenuItem>
+                <Divider />
                 <MenuItem className={classes.menu}>
                   <div className={classes.margin} />
                   <Slider
