33e91ad4a6f27738844c8e58138bef80593fdd19
Beena Ashish <86401125+Beenamol@users.noreply.github.com>
2023-06-14 16:47:44 +0530

    keep prev and next arrow in top in parallel bible page of mobile view  #363 (#378)
    keep prev and next arrow in top panel if parallel scroll mobile #363

--- src/components/read/Bible.js
+++ src/components/read/Bible.js
@@ -650,6 +650,9 @@ const Bible = (props) => {
     return <span className={classes[style]}>{" " + text}</span>;
   };
   const getPrevious = () => {
+    if(parallelScroll && paneNo === 1 && mobileView){
+      return " ";
+    }
     return previous && Object.values(previous).length !== 0 ? (
       <Tooltip title={previousBook + " " + previous.chapterId}>
         <ArrowBackIosIcon
@@ -663,6 +666,9 @@ const Bible = (props) => {
     );
   };
   const getNext = () => {
+    if(parallelScroll && paneNo === 1 && mobileView){
+      return " ";
+    }
     return next && Object.values(next).length !== 0 ? (
       <Tooltip title={nextBook + " " + next.chapterId}>
         <ArrowForwardIosIcon
