552b48f768c7fbf63d82f3ad1bbd1259acdd05f2
Pooja.Saini <pooja.saini@bridgeconn.com>
2023-11-27 10:33:08 +0530

    Change Login button to icon on top bar #495

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import firebase from "firebase/compat/app";
-import { connect } from "react-redux";
 import * as actions from "../../store/actions";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -8,12 +7,14 @@ import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Popover from "@material-ui/core/Popover";
 import Collapse from "@material-ui/core/Collapse";
 import Alert from "@material-ui/lab/Alert";
+import IconButton from "@material-ui/core/IconButton";
 import { useTranslation } from "react-i18next";
+import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -250,16 +251,18 @@ const Login = (props) => {
         </Button>
       ) : (
         <>
-          <Button
+          <IconButton
+            type="submit"
+            size="small"
             aria-describedby="sign-in"
             variant="contained"
-            size="small"
             ref={menuRef}
-            onClick={openForm}
             className={classes.signBtn}
+            onClick={openForm}
+            rel="noopener"
           >
-            {t("signInTitle")}
-          </Button>
+            <i className={`material-icons`}>person</i>
+          </IconButton>
           <Popover
             id="sign-in"
             open={open}
