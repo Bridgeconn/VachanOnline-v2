b94f7efd1fc7718d1f9c452377fcf7380244903a
Pooja.Saini <pooja.saini@bridgeconn.com>
2023-12-04 11:20:41 +0530

    added tooltip for help

--- src/components/common/Help.js
+++ src/components/common/Help.js
@@ -1,16 +1,21 @@
+import { Tooltip } from "@material-ui/core";
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 const Help = ({ mobileView, iconStyle, url }) => {
+  const { t } = useTranslation();
   return (
     <>
       {process.env.REACT_APP_DOCUMENT_URL ? (
         mobileView ? (
           ""
         ) : (
-          <Link to={process.env.REACT_APP_DOCUMENT_URL + url} target="_blank">
-            <i className={`material-icons ${iconStyle}`}>help_outline</i>
-          </Link>
+          <Tooltip title={t("landingHelpBtn")}>
+            <Link to={process.env.REACT_APP_DOCUMENT_URL + url} target="_blank">
+              <i className={`material-icons ${iconStyle}`}>help_outline</i>
+            </Link>
+          </Tooltip>
         )
       ) : (
         ""
